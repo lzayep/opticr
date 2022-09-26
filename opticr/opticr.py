e9a8aec5246497e40b8e7a2f9864cd1970bf347b
lzayep <ec@lza.sh>
2022-09-26 15:52:18 +0200

    Add an OCR abstract class to implement and init real tests cases

--- opticr/opticr.py
+++ opticr/opticr.py
@@ -1,26 +1,23 @@
-from pathlib import PurePath
+from typing_extensions import TypeAlias
 
-from PIL import Image
-import pytesseract
-import pdf2image
+from .ocr.tesseract import TesseractOcr
+from .ocr.googlevision import GoogleVisionOcr
+
+
+OCR: TypeAlias = TesseractOcr | GoogleVisionOcr
 
 
 class OpticR:
-    def __init__(self) -> None:
-        pass
+    processors: dict[str, type[OCR]] = {
+        "tesseract": TesseractOcr,
+        "google-vision": GoogleVisionOcr,
+    }
+
+    def __init__(self, processor: str = "tesseract") -> None:
+        self.processor: OCR = self.processors[processor]()
 
     def get_pages(self, filepath: str) -> list[str]:
-        _ = filepath
-        return []
+        return self.processor.get_pages(filepath)
 
-    def ocr(self, document_path: str) -> list[str]:
-        path = PurePath(document_path)
-        if path.suffix == ".pdf":
-            pages = pdf2image.convert_from_path(str(path))
-        else:
-            pages = [Image.open(str(path))]
-        pages_ocr = []
-        for page in pages:
-            pages_ocr.append(pytesseract.image_to_string(page))
-
-        return pages_ocr
+    def processor_name(self) -> str:
+        return self.processor.name
